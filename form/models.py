7f32403c279bf79addd7fdcb3d5c99781ab17805
Amitt Bhardwj <amitt.bhardwj@gmail.com>
2014-08-08 22:15:23 +0530

    Resume App

--- form/models.py
+++ form/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class resume(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    phone_no = models.IntegerField
-    address = models.TextField
+    phone_no = models.IntegerField(max_length=15)
+    address = models.TextField(max_length=500)
     email_id = models.EmailField(max_length=254)
-    gender = models.BooleanField
-
-
+    gender = models.CharField(max_length=10)
